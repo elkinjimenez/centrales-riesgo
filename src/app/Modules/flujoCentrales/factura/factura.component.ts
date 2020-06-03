1956b6da2e93f5dd4904fb34bf0d0d6856694bb5
Carlos Arturo Vargas Rodriguez <ECM0210H@claro.co>
2020-06-03 11:07:18 -0500

    Consumo Payments Factura

--- src/app/Modules/flujoCentrales/factura/factura.component.ts
+++ src/app/Modules/flujoCentrales/factura/factura.component.ts
@@ -4,6 +4,7 @@ import { RespPaymentreferences } from 'src/app/Models/flujoCentrales/resp-paymen
 import { ServiciosjavaService } from 'src/app/Services/serviciosjava.service';
 import { UtilService } from 'src/app/Services/util.service';
 import { Subscription } from 'rxjs';
+import { ReqPaymentreferences } from 'src/app/Models/flujoCentrales/req-paymentreferences';
 
 @Component({
   selector: 'app-factura',
@@ -27,6 +28,34 @@ export class FacturaComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
   }
+
+  consumirPaymentReferences() {
+    const errorM = 'Error al consultar factura.';
+    const request = {
+      headerRequest: {
+        channel: '',
+        idApplication:'',
+      },
+      subscriberNumber: ''
+    } as ReqPaymentreferences;
+    this.subscription = this.servicios.getPaymentReferences(errorM, request).subscribe(
+      data => {
+        console.log('Datos Factura: ', data);
+        this.responseGeneral = data as RespGeneral;
+        this.responsepaymentreferences = JSON.parse(this.responseGeneral.message);
+        if (this.responseGeneral.isValid) {
+          if(this.responsepaymentreferences.responseStatus.message === 'Transaction Complete'){
+              console.log('paso por aqui');
+          }
+        } else {
+
+        }
+      }, error => {
+        console.log('Error factura: ', error);
+      },
+    );
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
